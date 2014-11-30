b8426809c100e61e6512cc5100726d6a7c812b5b
jaodenazare <renatoz07@yahoo.com.br>
2014-11-30 20:43:20 -0200

    Update campeonato.h

--- src/campeonato.h
+++ src/campeonato.h
@@ -42,7 +42,7 @@ void OrdenarTabela(Time *v);
 void ImprimirTabela(Campeonato *c);
 void ImprimirTime(Campeonato *c, char *ntime);
 void ImprimirArtilheiro(Campeonato *c);
-void  OrdenarArtilheiros(Time *v,int tamanho);
+void OrdenarArtilheiros(Time *v,int tamanho);
 
 
 #endif // CAMPEONATO_H_INCLUDED
